1f65aa51d0142bd4a69010cdf4996aa3aad7fbb0
Dean Brown <dean@debian.homenet>
2024-07-27 19:59:05 +0100

    Format the date and the amount in the table

--- src/components/expenses/data-table/columns.tsx
+++ src/components/expenses/data-table/columns.tsx
@@ -1,9 +1,17 @@
-import {ColumnDef} from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 
 const columns: ColumnDef<Expense>[] = [
   {
     accessorKey: "expenseDate",
     header: "Date",
+    cell: ({ row }) => {
+      const date: Date = row.getValue("expenseDate");
+      const formattedDate: string = `${date.getDate()}/${
+        date.getMonth() + 1
+      }/${date.getFullYear()}`;
+      return <div>{formattedDate}</div>
+    },
+    
   },
   {
     accessorKey: "expenseName",
@@ -11,7 +19,15 @@ const columns: ColumnDef<Expense>[] = [
   },
   {
     accessorKey: "expenseAmount",
-    header: "Amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("expenseAmount"));
+      const formatted = new Intl.NumberFormat("en-IE", {
+        style: "currency",
+        currency: "EUR",
+      }).format(amount);
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
 ];
 
