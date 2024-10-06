0c8883611f2891b233ab91a9e4a2678be0071067
Dean Brown <dean@debian.homenet>
2024-10-06 13:10:32 +0100

    Add the database container to the docker-compose script. Update the app.ts file to use environment variables. Install dependencies for drizzle

--- backend/app.ts
+++ backend/app.ts
@@ -1,26 +1,30 @@
-import express, {Express, Request, Response} from "express";
-import cors, {CorsOptions} from "cors";
+import express, { Express, Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
 
-const port: string = "3000";
+// Create the app and set the port
+const port: string | undefined = process.env.PORT;
 const app: Express = express();
 
+// Parse requests with content-type application/json and x-www-form-urlencoded
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
+// Enable cors
 const allowedOrigins: string[] = ["http://localhost", "http://127.0.0.1"];
 const options: CorsOptions = {
   credentials: true,
   origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
 
 app.use(cors(options));
 
+// Create a test route
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).send({ message: "Hello World" });
+});
 
-app.get("/", async(req: Request, res: Response) => {
-  res.status(200).send({"message": "Hello World"});
-})
-
+// Start the application
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-})
+});
